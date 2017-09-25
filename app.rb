265bb7e357a7ddc0a5708ec75af70318ccf36f90
John Michael and Keegan Ruebling <keegan@>
2017-09-25 13:32:19 -0700

    push info for javi

--- app.rb
+++ app.rb
@@ -1,7 +1,45 @@
-require('sinatra')
-require('')
+require 'sinatra/activerecord'
+require 'sinatra'
+require 'pry'
+require 'sinatra/reloader'
+also_reload'.lib/**/*.rb'
+require './lib/division'
+require './lib/employee'
+require './lib/project'
+require 'pg'
+
 
 get('/') do
-
   erb(:index)
+end
+
+get('/hr') do
+  @employee = Employee.all()
+  @division = Division.all()
+  erb(:hr)
+end
+
+post('/division') do
+  title = params.fetch('title')
+  division = Division.new({:title => title})
+  division.save()
+  @division = Division.all()
+  @employee = Employee.all()
+  erb(:hr)
+end
+
+post('/employee') do
+  name = params.fetch('name')
+  employee = Employee.new({:name => name})
+  employee.save()
+  @division = Division.all()
+  @employee = Employee.all()
+  erb(:hr)
+end
+
+get('/division/:id') do
+  title = params.fetch('title')
+  @employee = Employee.all()
+
+  erb(:division_info)
 end
--- lib/project.rb
+++ lib/project.rb
@@ -1,6 +1,8 @@
 class Project < ActiveRecord::Base
   has_many(:employees)
+
   scope(:not_done, ->  do
     where({:done => false})
   end)
+
 end
